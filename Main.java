bc9ddc57b0bc34dec77ce364dfdd5cd3a5ce91e2
Javier <jramospesa@educacion.navarra.es>
2025-11-14 17:46:29 +0100

    Cambio de saludo en main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola soy Javier Ramos desde la version principal.");
+        System.out.println("Hola soy Javier Ramos desde la version principal");
 	System.out.println(“¡Que tengas un buen día!”);
     }
 }
